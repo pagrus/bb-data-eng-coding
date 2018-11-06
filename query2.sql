b8ae577b4be3272e7400278a62a34018fa97a8d4
pagrus <pagrus_marionette@nebulos.us>
2018-11-05 17:51:02 -0800

    update query for fix

--- query2.sql
+++ query2.sql
@@ -1,4 +1,10 @@
 .header on
 .mode csv
 
-SELECT p.item_name_a AS item_name, n.avg_diff AS colder, p.avg_diff AS warmer FROM item_change_pos_2f p JOIN item_change_neg_2f n ON p.item_name_a = n.item_name_a;
+SELECT pa.item_name, 
+CAST((neg_a_sum - neg_b_sum) AS REAL)/(SELECT COUNT(*) FROM temp_deltas WHERE diff = 2) AS "Avg change in sales when colder",
+CAST((pos_a_sum - pos_b_sum) AS REAL)/(SELECT COUNT(*) FROM temp_deltas WHERE diff = 2) AS "Avg change in sales when warmer"
+FROM pos_a_totals pa 
+JOIN pos_b_totals pb ON pa.item_name = pb.item_name
+JOIN neg_a_totals na ON pa.item_name = na.item_name
+JOIN neg_b_totals nb ON pa.item_name = nb.item_name;
